58cd29783123518ed9b31c455ca111a46f696972
001154815 <001154815@engelglobal.com>
2024-05-01 13:01:49 +0200

    first commit

--- apps/core/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "app.helipay.core"
--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-
-include("apps:bo")
-include("apps:cm")
-include("apps:core")
-include("apps:sm")
-include("apps:um")
-include("apps:am")
-include("apps:cn")
-
