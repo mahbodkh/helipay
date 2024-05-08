219f6db7a4916666be32236086f26d697d54f70b
001154815 <001154815@engelglobal.com>
2024-05-08 21:56:22 +0200

    added functionality

--- apps/core-engine/src/main/java/app/helipay/ce/Application.java
+++ apps/core-engine/src/main/java/app/helipay/ce/Application.java
@@ -3,11 +3,13 @@ package app.helipay.ce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "app.helipay.am", "app.helipay.bo", "app.helipay.be", "app.helipay.cm", "app.helipay.cn", "app.helipay.pm", "app.helipay.se", "app.helipay.sm", "app.helipay.um"
+})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
