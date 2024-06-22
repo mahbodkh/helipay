f04e1069e3baf340d7ae27293094827daef429ee
001154815 <001154815@engelglobal.com>
2024-06-22 22:29:48 +0200

    - added a reactjs ui - added MUI ( material ui )

---
+++ apps/webapp/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#333',
+    },
+    secondary: {
+      main: '#666',
+    },
+  },
+});
+
+export default theme;
