cf3cb290670c4fb2422a85c82092dda95f73d027
001154815 <001154815@engelglobal.com>
2024-06-22 22:29:48 +0200

    - added a reactjs ui - added MUI ( material ui )

--- apps/webapp/src/index.js
+++ apps/webapp/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MuiThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <App />
-    </MuiThemeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
